e6ff8cecf40e4cf50ab43fcd09cdbd27e7f3bd75
maylozq-sudo <maylozq@gmail.com>
2026-09-19 06:29:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ client.once('ready', () => {
   console.log(`Bot prêt ! Connecté en tant que ${client.user.tag}`);
 });
 
-client.login('MTU1MDY4OTExMTgxMDExNzY1Mg.GmAtBx.KFaypAwNRbWYVdXplRfsFtVVCxayMu5B0Y1OFw');
+client.login('MTU1MDY4OTExMTgxMDExNzY1Mg.GG_NN9.NLUmvI852u7Lk_BP5k6EogTLXDyWvgdgrn0pIM');
